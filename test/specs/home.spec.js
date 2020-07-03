158287075ef980e501ba88ea0641acfe0430395a
Pavan Sreenivasa <pavan.sreenivasa@thoughtspot.com>
2020-07-03 13:23:23 +0530

    removed binary

--- test/specs/home.spec.js
+++ test/specs/home.spec.js
@@ -2,13 +2,12 @@ import homePage from '../pages/home.page';
 import loginPage from '../pages/lookup.page';
 import lookupPage from '../pages/lookup.page';
 
-describe('Login Page Suite', function() {
-  it('Login to Shopify ', function() {
+describe('Login Page Suite', function () {
+  it('Login to Shopify ', function () {
     // Using component to capture repeating html part
     homePage
-        .open().updateButtonText('kaka-angadi.myshopify.com');
-        lookupPage.enterEmail('kulkarni.karthik@thinkify.io');
-        lookupPage.enterPassword('Shopify123');
-           
+      .open().updateButtonText('kaka-angadi.myshopify.com');
+    lookupPage.enterEmail('kulkarni.karthik@thinkify.io');
+    lookupPage.enterPassword('Shopify123');
   });
 });
